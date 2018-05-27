dd02e14a67c59857cf3725764ca44dd0fa856fdb
Moacir Moda <moa.moda@gmail.com>
2018-05-27 00:15:23 -0300

    fixing test profile

--- {{cookiecutter.project_slug}}/core/tests/test_model_profile.py
+++ {{cookiecutter.project_slug}}/core/tests/test_model_profile.py
@@ -5,6 +5,4 @@ from model_mommy import mommy
 class ModelTestCase(TestCase):
     def test_model_profile(self):
         user = mommy.make('User')
-        assert hasattr(user, 'created')
-        assert hasattr(user, 'updated')
         assert user.profile is not None
