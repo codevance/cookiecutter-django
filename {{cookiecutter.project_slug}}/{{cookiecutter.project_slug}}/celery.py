f6ec11023751fcb1c13f9de59dae3f68c15f6322
Moacir Moda <moa.moda@gmail.com>
2018-05-26 22:57:22 -0300

    adding celery

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/celery.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+from celery.schedules import crontab
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ cookiecutter.project_slug }}.settings')
+
+app = Celery('tasks')
+
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
+app.conf.beat_schedule = {
+    'import-auctions-from-today': {
+        'task': 'core.tasks.sum',
+        'schedule': crontab(hour=1),
+        'args': (2, 2),
+    },
+}
