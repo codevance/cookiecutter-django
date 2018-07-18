24d182b2abbefaaed1fbfe301086401b126374df
Moacir Moda <moa.moda@gmail.com>
2018-07-18 17:48:00 -0300

    logging hook

--- hooks/post_gen_project.sh
+++
@@ -1,3 +0,0 @@
-mv {{ cookiecutter.directory_name }}/* . >> /tmp/cookiecutter_post_gen.log 2>> /tmp/cookiecutter_post_gen.log
-mv {{ cookiecutter.directory_name }}/.* . >> /tmp/cookiecutter_post_gen.log 2>> /tmp/cookiecutter_post_gen.log
-rmdir {{ cookiecutter.directory_name }} >> /tmp/cookiecutter_post_gen.log 2>> /tmp/cookiecutter_post_gen.log
